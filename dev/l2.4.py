8da3e6e92a1037e09bdf0eb79a1b8a63ba24067b
Allen Mamaril <allen.mamaril@gmail.com>
2020-05-23 17:53:42 -0700

    Add code to save new processed image
    Add code to save new processed image and check-in test image

--- dev/l2.4.py
+++ dev/l2.4.py
@@ -13,14 +13,20 @@ xsize = image.shape[1]
 
 color_select = np.copy(image)
 
-red_threshhold = 0
-green_threshold = 0
-blue_threshold = 0
+red_threshhold = 200
+green_threshold = 200
+blue_threshold = 200
 rgb_threshold = [red_threshhold, green_threshold, blue_threshold]
 
 # Identify pixels below the threshold
-thresholds = (image[:,:,0] < rgb_threshold[0]) | (image[:,:,1] <rgb_threshold[1]) | (image[:,:,2] < rgb_threshold[2])
+thresholds = (image[:,:,0] < rgb_threshold[0]) \
+            | (image[:,:,1] < rgb_threshold[1]) \
+            | (image[:,:,2] < rgb_threshold[2])
+color_select[thresholds] = [0,0,0]
 
 # Display the image
 plt.imshow(color_select)
 plt.show()
+
+# Save processed image
+mpimg.imsave("test-after.png", color_select)
