5764fb17d1e4fe1a6d0bd7764b3717a762eacbfb
Allen Mamaril <allen.mamaril@gmail.com>
2020-05-26 16:30:24 -0700

    Update l2.7.py
    Change mask location

--- dev/l2.7.py
+++ dev/l2.7.py
@@ -4,41 +4,6 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-# Read in the image and print some stats
-image = mpimg.imread('test.jpg')
-print('This image is: ',type(image), 'with dimensions:', image.shape)
-
-# Pull out the x and y sizes and make a copy of the image
-ysize = image.shape[0]
-xsize = image.shape[1]
-color_select = np.copy(image)
-line_image = np.copy(image)
-
-# Define our color criteria
-red_threshold = 0
-green_threshold = 0
-blue_threshold = 0
-rgb_threshold = [red_threshold, green_threshold, blue_threshold]
-
-# Define a triangle region of interest (Note: if you run this code,
-# Keep in mind the origin (x=0, y=0) is in the upper left in image processing
-# you'll find these are not sensible values!!
-# But you'll get a chance to play with them soon in a quiz ;)
-left_bottom = [0,539]
-right_bottom = [900, 300]
-apex = [400, 0]
-
-# Fit lines (y=Ax+B) to identify the 3 sided region of interest
-# np.polyfit() returns the coefficients [A, B] of the Fit
-fit_left   = np.polyfit((left_bottom[0],  apex[0]), (left_bottom[1],  apex[1]), 1)
-fit_right  = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
-fit_bottom = np.polyfit((left_bottom[0],  right_bottom[0]), (left_bottom[1],  right_bottom[1]), 1)
-
-# Mask pixels below the threshold
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import numpy as np
-
 # Read in the image
 image = mpimg.imread('test.jpg')
 
@@ -52,18 +17,18 @@ color_select= np.copy(image)
 line_image = np.copy(image)
 
 # Define our color criteria
-red_threshold = 200
-green_threshold = 200
-blue_threshold = 200
+red_threshold = 0
+green_threshold = 0
+blue_threshold = 0
 rgb_threshold = [red_threshold, green_threshold, blue_threshold]
 
 # Define a triangle region of interest (Note: if you run this code,
 # Keep in mind the origin (x=0, y=0) is in the upper left in image processing
 # you'll find these are not sensible values!!
 # But you'll get a chance to play with them soon in a quiz ;)
-left_bottom = [0, 539]
-right_bottom = [900, 300]
-apex = [400, 0]
+left_bottom = [200, 719]
+right_bottom = [1100, 719]
+apex = [   625 , 400]
 
 fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
 fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
@@ -75,35 +40,18 @@ color_thresholds = (image[:,:,0] < rgb_threshold[0]) | \
                     (image[:,:,2] < rgb_threshold[2])
 
 # Find the region inside the lines
-# meshgrid returns coordinate matrices from coordinate vectors
 XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
-print('XX is: ', type(XX))
-print(XX)
-print('YY is: ', type(YY))
-print(YY)
 region_thresholds = (YY > (XX*fit_left[0] + fit_left[1])) & \
                     (YY > (XX*fit_right[0] + fit_right[1])) & \
                     (YY < (XX*fit_bottom[0] + fit_bottom[1]))
-print('region_thresholds is: ', type(region_thresholds))
-print('dimensions: ', region_thresholds.ndim)
-print('shape: ', region_thresholds.shape)
-print(region_thresholds)
-
 # Mask color selection
 color_select[color_thresholds] = [0,0,0]
-
-# Find Where image is both colored right and in the region
+# Find where image is both colored right and in the region
 line_image[~color_thresholds & region_thresholds] = [255,0,0]
 
-# Display the image
+# Display our two output images
 plt.imshow(color_select)
-plt.show()
 plt.imshow(line_image)
-plt.show()
 
 # uncomment if plot does not display
 plt.show()
-
-# Save processed image
-#mpimg.imsave("test-l2.7.png", color_select)
-#mpimg.imsave("test-l2.7.png", line_image)
