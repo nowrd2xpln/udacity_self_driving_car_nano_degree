b774edd2ca5b5850f98b48c7a999ca2cc4cb3d8c
Allen Mamaril <allen.mamaril@gmail.com>
2020-05-27 16:51:14 -0700

    Change threshold values

--- dev/l2.12.py
+++ dev/l2.12.py
@@ -17,15 +17,15 @@ plt.imshow(gray, cmap='gray')
 # Define a kernel size for Gaussian smoothing / blurring
 # Note: this step is optional as cv2.Canny() applies a 5x5 Gaussian internally
 # You can choose the kernel_size for Gaussian smoothing to be any odd number
-kernel_size = 7
+kernel_size = 5
 blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
 plt.imshow(blur_gray)
 #plt.show()
 
 # Define parameters for Canny and run it
 # NOTE: if you try running this code you might want to change these!
-low_threshold = 35
-high_threshold = 105
+low_threshold = 50
+high_threshold = 150
 edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 
 # Display the image
