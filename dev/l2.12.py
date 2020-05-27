1078477af535668990e3feb3e2ff7bbede2b3527
Allen Mamaril <allen.mamaril@gmail.com>
2020-05-27 16:06:56 -0700

    Apply gray scale conversion
    Apply gray scale conversion

--- dev/l2.12.py
+++ dev/l2.12.py
@@ -6,5 +6,10 @@ import cv2  #bringing in OpenCV libraries
 
 # Read in the image
 image = mpimg.imread('exit-ramp.jpg')
+
 plt.imshow(image)
+plt.show()
+
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) #grayscale conversion
+plt.imshow(gray, cmap='gray')
 plt.show()
