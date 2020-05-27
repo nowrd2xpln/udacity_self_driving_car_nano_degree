7511344ed4d262a26ca26d638dc96efdeb6b6ce5
Allen Mamaril <allen.mamaril@gmail.com>
2020-05-27 16:43:17 -0700

    Add OpenCV Canny filter
    Add OpenCV Canny filter

--- dev/l2.12.py
+++ dev/l2.12.py
@@ -7,9 +7,27 @@ import cv2  #bringing in OpenCV libraries
 # Read in the image
 image = mpimg.imread('exit-ramp.jpg')
 
-plt.imshow(image)
-plt.show()
+#plt.imshow(image)
+#plt.show()
 
 gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) #grayscale conversion
 plt.imshow(gray, cmap='gray')
+#plt.show()
+
+# Define a kernel size for Gaussian smoothing / blurring
+# Note: this step is optional as cv2.Canny() applies a 5x5 Gaussian internally
+# You can choose the kernel_size for Gaussian smoothing to be any odd number
+kernel_size = 7
+blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
+plt.imshow(blur_gray)
+#plt.show()
+
+# Define parameters for Canny and run it
+# NOTE: if you try running this code you might want to change these!
+low_threshold = 35
+high_threshold = 105
+edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+# Display the image
+plt.imshow(edges, cmap='Greys_r')
 plt.show()
