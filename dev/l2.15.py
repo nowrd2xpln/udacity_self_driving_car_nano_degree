780cd3c62f5169065c6bd4cfd3497ca6ef14c52c
Allen Mamaril <allen.mamaril@gmail.com>
2020-05-28 12:17:17 -0700

    Update debugging and comments

--- dev/l2.15.py
+++ dev/l2.15.py
@@ -31,29 +31,36 @@ masked_edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 
 # Define the Hough transform parameters
 # Make a blank the same size as our image to draw on
+# rho takes a minimum value of 1
+# a reasonable starting place for theta is 1 degree (pi/180 in radians)
+# Scale these values up to be more flexible in your definition of what constitutes a line.
+# threshold parameter specifies the minimum number of votes (intersections in a
+# given grid cell) a candidate line needs to have to make it into the output.
+# min_line_length is the minimum length of a line (in pixels) that you will accept in the output
+# max_line_gap is the maximum distance (again, in pixels) between segments that
+# you will allow to be connected into a single line.
 rho = 1
 theta = np.pi/180
 threshold = 1
 min_line_length = 10
 max_line_gap = 1
 line_image = np.copy(image)*0 #creating a blank to draw lines on
-print('line_image: ', type(line_image))
-print(line_image)
-
 
 # Run Hough on edge detected image
 lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]), \
                         min_line_length, max_line_gap)
-print('lines: ', type(lines))
-print(lines)
+print('lines: ', type(lines), '\n', lines)
 
 # Iterate over the output "lines" and draw lines on the blank
 for line in lines:
     for x1,y1,x2,y2 in line:
         cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+print('lines: ', type(lines), '\n', lines)
 
 # Create a "color" binary image to combine with line image
 color_edges = np.dstack((masked_edges, masked_edges, masked_edges))
+plt.imshow(color_edges)
+plt.show()
 
 # Draw the lines on the edge image
 combo = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0)
