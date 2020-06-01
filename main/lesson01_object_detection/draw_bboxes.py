2a3d26bc4cc15d418535c7df8b6162bc43db8d1f
Allen Mamaril <allen.mamaril@gmail.com>
2020-05-31 18:37:05 -0700

    Create rectangles on image
    Create rectangles on image

--- main/lesson01_object_detection/draw_bboxes.py
+++ main/lesson01_object_detection/draw_bboxes.py
@@ -15,6 +15,11 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
     # make a copy of the image
     draw_img = np.copy(img)
     # draw each bounding box on your image copy using cv2.rectangle()
+    for bbox in bboxes:
+        print bbox
+        cv2.rectangle(draw_img, bbox[0], bbox[1], color, thick)
+        print bbox[0]
+        print bbox[1]
     # return the image copy with boxes drawn
     return draw_img # Change this line to return image copy with boxes
 # Add bounding boxes in this format, these are just example coordinates.
