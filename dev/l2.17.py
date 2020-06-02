42c2e1730194ead291c6cd3456d46211953d8178
Allen Mamaril <allen.mamaril@gmail.com>
2020-06-02 10:07:21 -0700

    Change max gap threshold
    Change max gap threshold

--- dev/l2.17.py
+++ dev/l2.17.py
@@ -39,8 +39,9 @@ masked_edges = cv2.bitwise_and(edges, mask)
 rho = 1 # distance resolution in pixels of the Hough grid
 theta = np.pi/180 # angular resolution in radians of the Hough grid
 threshold = 1     # minimum number of votes (intersections in Hough grid cell)
+                  # points in image space need to be associated with each line segment
 min_line_length = 5 #minimum number of pixels making up a line
-max_line_gap = 1    # maximum gap in pixels between connectable line segments
+max_line_gap = 3    # maximum gap in pixels between connectable line segments
 line_image = np.copy(image)*0 # creating a blank to draw lines on
 
 # Run Hough on edge detected image
