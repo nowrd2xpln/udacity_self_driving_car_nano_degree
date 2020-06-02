9d743b0e5730e2c9121adb37a2729e31e809bc2b
Allen Mamaril <allen.mamaril@gmail.com>
2020-06-02 10:05:57 -0700

    Add LUV colorspace graph
    Add LUV colorspace graph

--- main/lesson01_object_detection/explore_color_spaces.py
+++ main/lesson01_object_detection/explore_color_spaces.py
@@ -44,6 +44,7 @@ img_small = cv2.resize(img, (np.int(img.shape[1] / scale), np.int(img.shape[0] /
 # Convert subsampled image to desired color space(s)
 img_small_RGB = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)  # OpenCV uses BGR, matplotlib likes RGB
 img_small_HSV = cv2.cvtColor(img_small, cv2.COLOR_BGR2HSV)
+img_small_LUV = cv2.cvtColor(img_small, cv2.COLOR_BGR2LUV)
 img_small_rgb = img_small_RGB / 255.  # scaled to [0, 1], only for plotting
 
 # Plot and show
@@ -51,4 +52,7 @@ plot3d(img_small_RGB, img_small_rgb)
 plt.show()
 
 plot3d(img_small_HSV, img_small_rgb, axis_labels=list("HSV"))
+plt.show()
+
+plot3d(img_small_LUV, img_small_rgb, axis_labels=list("LUV"))
 plt.show()
