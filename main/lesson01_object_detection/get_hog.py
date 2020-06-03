cbf602b6ec89cbe6faf66d735b2a8501ce9366bf
Allen Mamaril <allen.mamaril@gmail.com>
2020-06-03 14:04:51 -0700

    Add function to return HOG features and visualization
    Add function to return HOG features and visualization

--- main/lesson01_object_detection/get_hog.py
+++ main/lesson01_object_detection/get_hog.py
@@ -14,12 +14,27 @@ car_images = glob.glob('*.jpeg')
 # Features will always be the first element of the return
 # Image data will be returned as the second element if visualize= True
 # Otherwise there is no second return element
-
 def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=True,
                      feature_vec=True):
 
-    # TODO: Complete the function body and returns
-    pass
+    """
+    Function accepts params and returns HOG features (optionally flattened) and an optional matrix for
+    visualization. Features will always be the first return (flattened if feature_vector= True).
+    A visualization matrix will be the second return if visualize = True.
+    """
+
+    return_list = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block),
+                                  block_norm= 'L2-Hys', transform_sqrt=False,
+                                  visualise= vis, feature_vector= feature_vec)
+
+    # name returns explicitly
+    hog_features = return_list[0]
+    if vis:
+        hog_image = return_list[1]
+        return hog_features, hog_image
+    else:
+        return hog_features
 
 # Generate a random index to look at a car image
 ind = np.random.randint(0, len(car_images))
