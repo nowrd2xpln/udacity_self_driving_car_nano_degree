ff97e73e0954acd59eb994e3a9d0a6767ea5e917
Allen Mamaril <allen.mamaril@gmail.com>
2020-06-19 18:03:33 -0700

    Fix code for undistorting chessboard

--- main/lesson06_finding_corners/l6.18.py
+++ main/lesson06_finding_corners/l6.18.py
@@ -23,39 +23,51 @@ def corners_unwarp(img, nx, ny, mtx, dist):
     # Pass in your image into this function
     # Write code to do the following steps
     # 1) Undistort using mtx and dist
-    dst = cv2.undistort(img, mtx, dist, None, mtx)
-    cv2.imwrite('l6.18_output/test_undist.jpg',dst)
-
+    undst = cv2.undistort(img, mtx, dist, None, mtx)
     # 2) Convert to grayscale
-    img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-
+    img_gray = cv2.cvtColor(undst,cv2.COLOR_BGR2GRAY)
     # 3) Find the chessboard corners
     ret, corners = cv2.findChessboardCorners(img_gray, (nx,ny), None)
-
     # 4) If corners found:
+
     if ret == True:
-            # a) draw corners
-            cv2.drawChessboardCorners(img, (nx,ny), corners, ret)
-            cv2.imwrite('l6.18_output/corners_found.jpg', img)
-            cv2.imshow('img', img)
-            cv2.waitKey(1000)
-            print(corners)
-            print(len(corners))
+        # a) draw corners
+        cv2.drawChessboardCorners(undst, (nx,ny), corners, ret)
+        # b) define 4 source points src = np.float32([[,],[,],[,],[,]])
+             #Note: you could pick any four of the detected corners
+             # as long as those four corners define a rectangle
+             #One especially smart way to do this would be to use four well-chosen
+             # corners that were automatically detected during the undistortion steps
+             #We recommend using the automatic detection of corners in your code
+        src = np.float32([corners[0],
+                          corners[nx-1],
+                          corners[-1],
+                          corners[-nx]])
+        # src = np.float32([corners[7],
+        #                   corners[47],
+        #                   corners[40],
+        #                   corners[0]])
+        # c) define 4 destination points dst = np.float32([[,],[,],[,],[,]])
+        pad = pt = 50
+        cell = 100
+        img_size = (img.shape[1], img.shape[0])
+        # dst = np.float32([[pad, pad],
+        #                   [pad + cell*nx, pad],
+        #                   [ pad + cell*nx, pad + cell*ny],
+        #                   [ pad, pad + cell*ny]])
+        dst = np.float32([[pt,pt],
+                          [img.shape[1]-pt,pt],
+                          [img.shape[1]-pt,img.shape[0]-pt],
+                          [pt,img.shape[0]-pt]])
+        # d) use cv2.getPerspectiveTransform() to get M, the transform matrix
+        M = cv2.getPerspectiveTransform(src, dst)
 
-            # b) define 4 source points src = np.float32([[,],[,],[,],[,]])
-                 #Note: you could pick any four of the detected corners
-                 # as long as those four corners define a rectangle
-                 #One especially smart way to do this would be to use four well-chosen
-                 # corners that were automatically detected during the undistortion steps
-                 #We recommend using the automatic detection of corners in your code
-            src = np.float32([[,],[,],[,],[,]])
+        # e) use cv2.warpPerspective() to warp your image to a top-down view
+        warped = cv2.warpPerspective(undst, M, img_size, flags=cv2.INTER_LINEAR)
 
-            # c) define 4 destination points dst = np.float32([[,],[,],[,],[,]])
-            # d) use cv2.getPerspectiveTransform() to get M, the transform matrix
-            # e) use cv2.warpPerspective() to warp your image to a top-down view
     #delete the next two lines
-    M = None
-    warped = np.copy(img)
+    #M = None
+    #warped = np.copy(img)
     return warped, M
 
 top_down, perspective_M = corners_unwarp(img, nx, ny, mtx, dist)
@@ -66,3 +78,4 @@ ax1.set_title('Original Image', fontsize=50)
 ax2.imshow(top_down)
 ax2.set_title('Undistorted and Warped Image', fontsize=50)
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
